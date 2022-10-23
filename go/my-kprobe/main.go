d2a4bf6c08ebf05acd10869c10e59af3fa8445e3
Shunpoco <tkngsnsk313320@gmail.com>
2022-10-23 20:05:14 +0900

    Above

--- go/my-kprobe/main.go
+++ go/my-kprobe/main.go
@@ -16,7 +16,6 @@ import "C"
 const source string = `
 #include <uapi/linux/ptrace.h>
 #include <bcc/proto.h>
-
 typedef struct {
 	u32 pid;
 	uid_t uid;
@@ -24,12 +23,12 @@ typedef struct {
 	int ret;
 	char filename[256];
 } chown_event_t;
-
 BPF_PERF_OUTPUT(chown_events);
 BPF_HASH(chowncall, u64, chown_event_t);
-
-int kprobe__sys_fchownat(struct pt_regs *ctx, int dfd, const char *filename, uid_t uid, gid_t gid, int flag) {
-	bpf_trace_printk("hello, world!\\n");
+int kprobe__sys_fchownat(struct pt_regs *ctx, int dfd, const char *filename,
+                      uid_t uid, gid_t gid, int flag)
+{
+        bpf_trace_printk("Hello, world!\\n");
 	u64 pid = bpf_get_current_pid_tgid();
 	chown_event_t event = {
 		.pid = pid >> 32,
@@ -40,21 +39,20 @@ int kprobe__sys_fchownat(struct pt_regs *ctx, int dfd, const char *filename, uid
 	chowncall.update(&pid, &event);
 	return 0;
 }
-
-int kretprobe__sys_fchownat(struct pt_regs *ctx) {
+int kretprobe__sys_fchownat(struct pt_regs *ctx)
+{
 	int ret = PT_REGS_RC(ctx);
 	u64 pid = bpf_get_current_pid_tgid();
 	chown_event_t *eventp = chowncall.lookup(&pid);
 	if (eventp == 0) {
 		return 0;
 	}
-
 	chown_event_t event = *eventp;
 	event.ret = ret;
 	chown_events.perf_submit(ctx, &event, sizeof(event));
 	chowncall.delete(&pid);
 	return 0;
-}
+};
 `
 
 type chownEvent struct {

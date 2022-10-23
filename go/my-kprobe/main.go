b10aaf90606af5fa521f37ca5f9253d0492b5456
Shunpoco <tkngsnsk313320@gmail.com>
2022-10-23 11:04:19 +0000

    Revert "Use original source"
    This reverts commit 8380d786ee9787575123bd355470a0237af2044b.

--- go/my-kprobe/main.go
+++ go/my-kprobe/main.go
@@ -16,6 +16,7 @@ import "C"
 const source string = `
 #include <uapi/linux/ptrace.h>
 #include <bcc/proto.h>
+
 typedef struct {
 	u32 pid;
 	uid_t uid;
@@ -23,12 +24,12 @@ typedef struct {
 	int ret;
 	char filename[256];
 } chown_event_t;
+
 BPF_PERF_OUTPUT(chown_events);
 BPF_HASH(chowncall, u64, chown_event_t);
-int kprobe__sys_fchownat(struct pt_regs *ctx, int dfd, const char *filename,
-                      uid_t uid, gid_t gid, int flag)
-{
-        bpf_trace_printk("Hello, world!\\n");
+
+int kprobe__sys_fchownat(struct pt_regs *ctx, int dfd, const char *filename, uid_t uid, gid_t gid, int flag) {
+	bpf_trace_printk("hello, world!\\n");
 	u64 pid = bpf_get_current_pid_tgid();
 	chown_event_t event = {
 		.pid = pid >> 32,
@@ -39,20 +40,21 @@ int kprobe__sys_fchownat(struct pt_regs *ctx, int dfd, const char *filename,
 	chowncall.update(&pid, &event);
 	return 0;
 }
-int kretprobe__sys_fchownat(struct pt_regs *ctx)
-{
+
+int kretprobe__sys_fchownat(struct pt_regs *ctx) {
 	int ret = PT_REGS_RC(ctx);
 	u64 pid = bpf_get_current_pid_tgid();
 	chown_event_t *eventp = chowncall.lookup(&pid);
 	if (eventp == 0) {
 		return 0;
 	}
+
 	chown_event_t event = *eventp;
 	event.ret = ret;
 	chown_events.perf_submit(ctx, &event, sizeof(event));
 	chowncall.delete(&pid);
 	return 0;
-};
+}
 `
 
 type chownEvent struct {
@@ -64,35 +66,37 @@ type chownEvent struct {
 }
 
 func main() {
-	module := bpf.NewModule(source, []string{})
-	defer module.Close()
+	m := bpf.NewModule(source, []string{})
+	defer m.Close()
 
+	// Specify a kernel function
 	fnName := bpf.GetSyscallFnName("fchownat")
 
-	kprobe, err := module.LoadKprobe("kprobe__sys_fchownat")
+	// Load user-defined C function to Kprobe
+	kprobe, err := m.LoadKprobe("kprobe__sys_fchownat")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to laod syscall__execve: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Failed to load syscall__execve: %s\n", err)
 		os.Exit(1)
 	}
 
-	if err := module.AttachKprobe(fnName, kprobe, -1); err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to attach syscall__execve: %s\n", err)
+	// Attach Kprobe to the specific kernel function
+	if err := m.AttachKprobe(fnName, kprobe, -1); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to attach syscall_execve: %s\n", err)
 		os.Exit(1)
 	}
 
-	retKprobe, err := module.LoadKprobe("kretprobe__sys_fchownat")
+	retKprobe, err := m.LoadKprobe("kretprobe__sys_fchownat")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to load kretprobe__sys_fchownat: %s\n", err)
+		fmt.Fprintf(os.Stderr, "Failed to load kretprobe__sys_chownat: %s\n", err)
 		os.Exit(1)
 	}
 
-	err = module.AttachKretprobe(fnName, retKprobe, -1)
-	if err != nil {
+	if err := m.AttachKprobe(fnName, retKprobe, -1); err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to attach kretprobe__sys_fchownat: %s\n", err)
 		os.Exit(1)
 	}
 
-	table := bpf.NewTable(module.TableId("chown_events"), module)
+	table := bpf.NewTable(m.TableId("chown_events"), m)
 
 	channel := make(chan []byte)
 
@@ -104,6 +108,8 @@ func main() {
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, os.Kill)
+
+	fmt.Println("start.")
 
 	go func() {
 		var event chownEvent
@@ -117,7 +123,6 @@ func main() {
 			filename := (*C.char)(unsafe.Pointer(&event.Filename))
 			fmt.Printf("uid %d gid %d pid %d called fchownat(2) on %s (return value: %d)\n", event.Uid, event.Gid, event.Pid, C.GoString(filename), event.ReturnValue)
 		}
-
 	}()
 
 	perfMap.Start()
